a312e646c4230a4bc2bf8fd4ae497e2d7f621fa9
MarcoVitoC <marcovito.chr@gmail.com>
2023-12-01 22:55:23 +0700

    chore: test push third time

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		System.out.println("Test Push");
 		System.out.println("Marco is testing git push...");
 		System.out.println("Marco is testing git push again");
+		System.out.println("Marco is testing git push for the third time");
 	}
 
 	public static void main(String[] args) {
