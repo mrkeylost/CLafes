f82ac45fb5a688d0ef9d99866c092839d300b2d5
MarcoVitoC <marcovito.chr@gmail.com>
2023-12-01 22:11:06 +0700

    chore: test push

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public Main() {
 		System.out.println("Test Push");
+		System.out.println("Marco is testing git push...");
 	}
 
 	public static void main(String[] args) {
--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,8 @@
-module Clafes {
+/**
+ * 
+ */
+/**
+ * 
+ */
+module CLafes {
 }
